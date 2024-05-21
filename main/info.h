f0291edacf4b43391d65d4b5d9cd6c42be429acc
Kevin Lai <laijunhui233@gmail.com>
2024-05-21 12:42:37 +0800

    english comments

--- main/info.h
+++ main/info.h
@@ -1,39 +1,36 @@
 #pragma once
 #include "info_advanced.h"
 
-/* ---> 遙控車設定 <--- */
+/* ---> Remote Control Car Settings <--- */
 
-// 當遙控車與遙控器斷開連接後，多少秒後自動關機
-// 單位: 秒 - s
-// 範圍: 10 to 100
-// 默認: 10
+// When the remote control car disconnects from the remote controller, 
+// how many seconds until it automatically shuts down
+// Unit: seconds (s)
+// Range: 10 to 100
+// Default: 10
 #define IDLE_SHUTDOWN_SECONDS 10
 
-#if (CAR_USE_PID_CONTROL == false)
-
-// 左車輪PWM
-// 單位: 百分比 - %
-// 範圍: 25 to 100
-// 默認: 35
+// Left wheel PWM
+// Unit: percentage - %
+// Range: 25 to 100
+// Default: 35
 #define LEFT_MOTOR_POWER 35
 
-// 右車輪PWM
-// 單位: 百分比 - %
-// 範圍: 25 to 100
-// 默認: 35
+// Right wheel PWM
+// Unit: percentage - %
+// Range: 25 to 100
+// Default: 35
 #define RIGHT_MOTOR_POWER 35
 
-#endif
-
-// 設定後，遙控車將會剎車而非滑行
-// 可選: true, false
-// 默認: false
+// When set, the remote control car will brake instead of coasting
+// Options: true, false
+// Default: false
 #define MOTOR_BRAKE_ON_IDLE false
 
-/* ---> 投射器設定 <--- */
+/* ---> Catapult Setting <--- */
 
-// 激光強度
-// 單位: 百分比 - %
-// 範圍: 0 to 100
-// 默認: 10
+// Laser intensity
+// Unit: percentage - %
+// Range: 0 to 100
+// Default: 10
 #define LASER_POWER 10
--- main/info_advanced.h
+++ main/info_advanced.h
@@ -1,25 +1,25 @@
 #pragma once
 
-/* ---> 遙控車設定 <--- */
+/* ---> Remote Control Car Settings <--- */
 
-// 遙控車有守門模組
-// 可選: true, false
-// 默認: false
+// Does the remote control car have a goalkeeper module?
+// Options: true, false
+// Default: false
 #define HAS_GOALKEEPER_MODULE true
 
-// 遙控車有投射器模組
-// 可選: true, false
-// 默認: false
+// Does the remote control car have a catapult module?
+// Options: true, false
+// Default: false
 #define HAS_CATAPULT_MODULE true
 
-// 使用PID控制
-// 可選: true, false
-// 默認: false
+// Use PID control for the remote control car?
+// Options: true, false
+// Default: false
 #define CAR_USE_PID_CONTROL false
 
-// 將遙控車PID除錯訊息經由Wi-Fi發送至遙控器
-// 可選: true, false
-// 默認: false
+// Transmit PID debugging messages from the remote control car to the controller via Wi-Fi?
+// Options: true, false
+// Default: false
 #define DEBUG_TRANSMIT_PID_STATUS_TO_REMOTE false
 
 #if (CAR_USE_PID_CONTROL == false)
@@ -36,52 +36,52 @@
 
 #endif
 
-// 默認車輪PWM頻率
-// 單位: 頻率 - Hz
-// 範圍: 20 to 20000
-// 默認: 500
+// Default motor PWM frequency
+// Unit: frequency - Hz
+// Range: 20 to 20000
+// Default: 500
 #define MOTOR_PWM_FREQUENCY 500
 
 // ???
-// 可選: true, false
-// 默認: false
+// Options: true, false
+// Default: false
 #define ENABLE_MUSIC_PLAYER false
 
-/* ---> Wi-Fi設定 <--- */
+/* ---> Wi-Fi Settings <--- */
 
-// 每隔3秒列出Wi-Fi連線狀態
-// 可選: true, false
-// 默認: true
+// Show Wi-Fi connection status every 3 seconds
+// Options: true, false
+// Default: true
 #define SHOW_CONNECTION_STATUS false
 
-// 上傳新代碼時清除舊連線 (Clears ROM)
-// 可選: true, false
-// 默認: true
+// Clear old connections when uploading new code (Clears ROM)
+// Options: true, false
+// Default: true
 #define CLEAR_PAIRED_PEER_ON_NEW_UPLOAD true
 
-// 訊號強度大於該值時將嘗試建立連線
-// 單位: 分貝毫瓦 - dBm
-// 範圍: 0 to -100
-// 默認: -20
+// Attempt to establish a connection when signal strength is greater than this value
+// Unit: decibel-milliwatts - dBm
+// Range: 0 to -100
+// Default: -20
 #define MIN_RSSI_TO_INITIATE_CONNECTION -20
 
-/* ---> 內置彩色LED設定 <--- */
+/* ---> Built-in RGB LED Settings <--- */
 // https://www.selecolor.com/en/hsv-color-picker/
 
-// 內置彩色LED - 色相
-// 單位: 角度/相位 - °
-// 範圍: 0 to 360
-// 默認: 350
+// Built-in RGB LED - Hue
+// Unit: angle/phase - °
+// Range: 0 to 360
+// Default: 350
 #define RGB_LED_HUE 350
 
-// 內置彩色LED - 飽和度
-// 單位: 百分比 - %
-// 範圍: 0 to 100
-// 默認: 75
+// Built-in RGB LED - Saturation
+// Unit: percentage - %
+// Range: 0 to 100
+// Default: 75
 #define RGB_LED_SATURATION 75
 
-// 內置彩色LED - 明度
-// 單位: 百分比 - %
-// 範圍: 0 to 100
-// 默認: 10
+// Built-in RGB LED - Brightness / Value
+// Unit: percentage - %
+// Range: 0 to 100
+// Default: 10
 #define RGB_LED_VALUE 10
